ca1f180dec743b03dc33a324d597bcf40505247a
George Bockari <bitwhys@gmail.com>
2022-11-02 20:29:23 -0400

    refactor: add publishConfig key to allow publishing to verdaccio

--- scripts/preparePackageJson.js
+++ scripts/preparePackageJson.js
@@ -3,14 +3,20 @@ const fs = require('fs')
 const packageJson = require('../package.json')
 
 const DIST_DIR = path.resolve(__dirname, '..', 'package')
+const publishConfig = {
+  publishConfig: {
+    registry: 'http://localhost:4873',
+  },
+}
 
 delete packageJson.devDependencies
 delete packageJson.config
 delete packageJson.scripts.cz
 delete packageJson.scripts.build
 
+const formattedPackageJson = { ...packageJson, ...publishConfig }
+
 fs.writeFileSync(
   `${DIST_DIR}/package.json`,
-  JSON.stringify(packageJson, null, 2),
-  {}
+  JSON.stringify(formattedPackageJson, null, 2)
 )
